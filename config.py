889251521bec5bf9ad9ab563a17a757bf7153943
xiaoyongyong <1045235634@qq.com>
2019-12-12 11:03:11 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+WEBSOCKET_URL='wss://w1.chainx.org.cn/ws'
+# cybextest的公钥地址
+Pubkey='0x623a6904ec19654e53a1b887d14365cf86fe75906d9110c428f9173cc5f6fecd'
+BASE = 0.5 * (10**8)
+Precision = 4
+# cybextest的私钥地址
+PRIVK='0x21e62b46676631fb092457af2e5347ea9f51fe5eaeb7c01fb3e6e38b9161c590'
+Fee = 0.5 * (10**8)
+# 本地mongodb的数据库URL和NAME
+MONGODB_DB_URL ='mongodb://test:test123@localhost:27017/mongotest'
+MONGODB_DB_NAME='mongotest'
+Alpha = 0.8
+# 最后剩余的钱要转入的目标账号的公钥
+Transfer_to='0x86e7
